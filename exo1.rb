c81c851efa45898d8ed822f59bd095ce7d8993de
Olivier-Kango <olivierkango@gmail.com>
2022-12-11 16:45:51 -0800

    Add gets.chomp and .even?

---
+++ exo1.rb
@@ -0,0 +1,9 @@
+# puts "Salut la planète"
+# pour exécuté c'est ruby exo1.rb
+# puts "Entrez votre nom :"
+# nom = gets.chomp
+# puts "Salut #{nom} comment allez vous ?"
+
+puts "Quelle est votre âge?"
+age = gets.chomp.to_i.even?
+puts "#{age}"

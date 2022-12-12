9001f5f3b5977a4e72d032474792322bdcc4971b
Olivier-Kango <olivierkango@gmail.com>
2022-12-11 16:53:30 -0800

    Simple condition

--- exo1.rb
+++ exo1.rb
@@ -4,6 +4,15 @@
 # nom = gets.chomp
 # puts "Salut #{nom} comment allez vous ?"
 
+# puts "Entrez un chiffre"
+# chiffre = gets.chomp
+# puts chiffre.to_i.even?
+
+# Les conditions
 puts "Entrez un chiffre"
-chiffre = gets.chomp
-puts chiffre.to_i.even?
+chiffre = gets.chomp.to_i
+if chiffre.even?
+  puts "Votre chiffre est pair"  
+else
+  puts "Votre chiffre est impair"
+end

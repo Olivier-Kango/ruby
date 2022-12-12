d765bf1a2bae0ec5b7659a714cd7c1c1206fd9e9
Olivier-Kango <olivierkango@gmail.com>
2022-12-11 17:39:02 -0800

    if and unless

--- exo1.rb
+++ exo1.rb
@@ -8,11 +8,31 @@
 # chiffre = gets.chomp
 # puts chiffre.to_i.even?
 
+
 # Les conditions
-puts "Entrez un chiffre"
+
+# puts "Entrez un chiffre"
+# chiffre = gets.chomp.to_i
+# if chiffre.even?
+#   puts "Votre chiffre est pair"  
+# else
+#   puts "Votre chiffre est impair"
+# end
+
+
+# Conditions un peu complexe
+
+puts "Entrez un chiffre:"
 chiffre = gets.chomp.to_i
-if chiffre.even?
-  puts "Votre chiffre est pair"  
+adeviner = 15
+
+# if chiffre < adeviner
+#   puts "#{chiffre} est trop petit :("
+# elsif chiffre > adeviner
+#   puts "#{chiffre} est trop grand :("
+# if chiffre != adeviner
+unless chiffre == adeviner
+  puts "Tu as perdu :("
 else
-  puts "Votre chiffre est impair"
+  puts "Bravo tu as deviné ! :)"
 end
